44114e337bdc234e1750cfb46c14cebc51b3442d
Gerardo Rocha <gerardor@auctionedge.com>
2021-10-20 23:27:58 -0500

    Add tweet model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :tweets
 
   validates :username, length: { in: 2..10 }
   validates :username, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_18_230245) do
+ActiveRecord::Schema.define(version: 2021_10_20_235735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+    t.integer "likes"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_10_18_230245) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tweets", "users"
 end
